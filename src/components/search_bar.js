6c45fd24cb1ecdf8a195a8b19eaf7eafcf47543b
Muntaeyeon <monty420420@naver.com>
2021-11-20 00:33:57 +0900

    react_concept

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,11 @@
  import React, { Component } from 'react';
 
- //함수형 컴포넌트
+ //함수형 컴포넌트 : 어떤 정보와 jsx로 분리될 때 주로 이용
 /*const SearchBar = () => {
     return <input />;
 };*/
 
-//클래스형 컴포넌트
+//클래스형 컴포넌트 : 스테이트를 파악할 필요가 있을때 사용(데이터가 바뀌고 있거나 이벤트반응 랜더링과정)
 class SearchBar extends Component {
     constructor(props) {
         super(props);
